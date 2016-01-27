9581957982835b31bd11d96f711c78ae805380fc
ehebrard <emmanuel.hebrard@gmail.com>
2016-01-27 14:55:19 +0100

    experiments

--- runexp.py
+++ runexp.py
@@ -86,7 +86,7 @@ def run_correlation_experiments(typecor, cutoff):
     
     stop = False
     
-    methods = ['no', 'gcc', 'sort']
+    methods = ['no', 'gcc'] #, 'sort']
     
     length = 5
     
@@ -107,7 +107,7 @@ def run_correlation_experiments(typecor, cutoff):
             
                 cmdline = [cmd for cmd in chocoprefix]
                 
-                cmdargs = [str(length), str(False), typecor, method, str(False), str(cutoff), str(False), '12345', '8', '20']
+                cmdargs = [str(length), str(False), typecor, method, str(False), str(cutoff), str(False), '12345', '8', '50']
                 
                 #cmdline.extend(['--rank', '--time', str(cutoff), '--length', str(length), '--type', typecor, '--show', 'statistics', '--decomp', method])
                 cmdline.extend( cmdargs )
@@ -132,7 +132,7 @@ if __name__ == '__main__':
         run_cmdline(get_cmdline())
     else:
         print "run experiments"
-        run_correlation_experiments('uncorrelation', 36000000)
+        run_correlation_experiments('uncorrelation', 100000)
 
 
 
